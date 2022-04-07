f3256eb8528a174ed114eb28edc1c264afbe6340
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2022-04-07 13:06:43 -0300

    feat(prog): finish ch 5 rust

--- Programming/the_rust_programming_language/chapter_5/method_syntax/src/main.rs
+++ Programming/the_rust_programming_language/chapter_5/method_syntax/src/main.rs
@@ -1,3 +1,56 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+
+    // Associated functions
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    // Methods
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let rec = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rec_1 = Rectangle {
+        width: 29,
+        height: 49,
+    };
+
+    println!("The rectangle area is {}", rec.area());
+
+    if rec.width() {
+        println!("The rectangle width is {}", rec.width);
+    }
+
+    println!("The rectangle rec can hold rectangle 1? {}", rec.can_hold(&rec_1));
+
+    let sqr = Rectangle::square(10);
+
+    println!("The new square created has the length of {}", sqr.width);
+
 }
